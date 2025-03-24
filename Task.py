5ac290119df39de7e60f06ce157ab9141e9378dc
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-03-24 21:32:12 +0700

    ready
    готово

--- Task.py
+++ Task.py
@@ -20,6 +20,13 @@ class Task_manager:
             if task.status == False:
                 print(f'Описание из new: {task.describe}, срок: {task.term}, выполнение: {task.status}')
 
+    def find_task(self, describe):
+        for task in self.task_list:
+            if task.describe == describe:
+                task.status == True
+        print(f'Описание из find_task: {task.describe}, срок: {task.term}, выполнение: {task.status}')
+
+
     def show_task(self,):
         list_box.delete(0, tk.END) # Очищаем Listbox (list_box) перед загрузкой нового содержимого
         for task in self.task_list:
@@ -27,7 +34,7 @@ class Task_manager:
                 print(f'Описание: {task.describe}, срок: {task.term}, выполнение: {task.status}')
                 stroka = task.describe + ' до ' + task.term
                 list_box.insert(tk.END, stroka.strip())   #  list_box.append(stroka)
-                print('stroka:',stroka)
+                # print('stroka:',stroka)
         return list_box
 
 def add_item(): # по кнопке добавить задачу
@@ -36,13 +43,22 @@ def add_item(): # по кнопке добавить задачу
         list_box.insert(tk.END, task) # встаавляем значение в конец списка list_box
         task_entry.delete(0, tk.END) # очищаем поле ввода
         descibe, term = task.split(',')
-
         manager.new_task(descibe, term, False)
 
+def del_item(): # по кнопке удалиь задачу
+    select_task_index = list_box.curselection()
+    if select_task_index:
+        task_content = list_box.get(select_task_index)  # Получаем текст выбранной задачи
+        list_box.delete(select_task_index)
+        # task_content = task_content.s
+        manager.find_task(task_content)
+        # descibe, term = select_task.split(',')
+        print('task_content:',task_content)
+
 manager = Task_manager() # создаем экземпляр менеджера задач
-manager.new_task('Cделать задание по ООП', '24/03', False)
-manager.new_task('Прочитать книгу по ООП', '24/03', False)
-manager.new_task('Получить консультацию по ООП', '24/03', True)
+manager.new_task('Cделать задание по ООП,', '24/03', False)
+manager.new_task('Прочитать книгу по ООП,', '24/03', False)
+manager.new_task('Получить консультацию по ООП,', '24/03', True)
 
 print()
 # describe = input('Ведите описание задачи - ')
@@ -70,7 +86,7 @@ list_box.pack(side=tk.LEFT, pady=0)
 add_botton = tk.Button(frame, bg='AntiqueWhite2', text='Добавить', command=add_item) #manager.new_task
 add_botton.pack(pady=5)
 
-add_botton = tk.Button(frame, bg='AntiqueWhite2', text='Удалить', command=)
+add_botton = tk.Button(frame, bg='AntiqueWhite2', text='Удалить', command=del_item)
 add_botton.pack(side=tk.BOTTOM,pady=5)
 
 # add_botton = tk.Button(frame, bg='AntiqueWhite2', text='Добавить', command=)
